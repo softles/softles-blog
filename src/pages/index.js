bb4e02ffacc66d1aa2a943c75d929ad3f73160ff
gurjarshakti <mr.shakti.singh33@gmail.com>
2019-11-30 02:31:20 +0530

    new changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,56 +1,32 @@
 import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import '../styles/index.css';
+import Button from '@material-ui/core/Button';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
 
-function Index() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
-  return (
-    <main>
-      <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
-      </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ZEIT Now
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/zeit/now-examples/blob/master/gatsby-functions"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
-      </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
-    </main>
-  );
-}
 
-export default Index;
+export default () => (
+  <div style={{ color: `purple`, fontSize: `72px` }}>
+  <h1>Hello Gatsby!</h1>
+  <p>What a world.</p>
+  <img src="https://source.unsplash.com/random/400x200" alt="" />
+  <div>
+  <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  </div>
+  <div className="row">
+   <div className="col">
+    <p>OK oK</p>
+    <p> ok ok</p>
+   </div>  
+   <div className="col">
+   <p>OK oK</p>
+   <p> ok ok</p>
+   </div>
+  </div>
+  </div>
+)
+
+
+
